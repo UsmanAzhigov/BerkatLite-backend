8713da029895caa56bbd2970aa8f01ddd0949a17
Archakov <ali.azhigov@gmail.com>
2025-07-27 21:43:22 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'http://berkat-lite.ru',
+    origin: ['http://berkat-lite.ru', 'http://188.225.72.89'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
